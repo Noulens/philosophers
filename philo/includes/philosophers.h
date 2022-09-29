8a58ad0d9e9da2bd68c1003de5d57a48bb747a37
Thibault NOULENS <tnoulens@e1r12p9.clusters.42paris.fr>
2022-09-29 19:29:30 +0200

    makefile

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 18:56:16 by tnoulens          #+#    #+#             */
+/*   Updated: 2022/09/29 19:00:52 by tnoulens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <signal.h>
+# include <linux/limits.h>
+# include <limits.h>
+# include <errno.h>
+# include "colors.h"
+
+#endif
