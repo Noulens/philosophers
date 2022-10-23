28ca04545dace8dd20221b6134e94f0b40dad460
Thibault NOULENS <tnoulens@e1r1p1.clusters.42paris.fr>
2022-10-24 00:38:33 +0200

    philo 23-10

--- philo/srcs/diner.c
+++ philo/srcs/diner.c
@@ -6,21 +6,11 @@
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:25:30 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/10/23 21:54:45 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/10/24 00:33:33 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
-
-void	*diner(void *a)
-{
-	t_simulation	*b;
-
-	b = (t_simulation *)a;
-	if (inittime(b) == 1)
-		return (NULL);
-	return (NULL);
-}
 
 void	sleeping(t_simulation *sm)
 {
@@ -33,4 +23,46 @@ void	sleeping(t_simulation *sm)
 			break ;
 		usleep(10);
 	}
+}
+
+void	die(t_simulation *sm)
+{
+	time_t	die_time;
+
+	die_time = gettimeinms() + sm->ttd;
+	while (gettimeinms() < die_time)
+	{
+	}
+	printf("%ld: %d died\n", gettimeinms() - sm->start, sm->philo[0]->num);
+}
+
+void	*thread_routine_one(void *a)
+{
+	t_simulation	*sm;
+
+	sm = (t_simulation *)a;
+	pthread_mutex_lock(&sm->forks[0]->fork);
+	printf("%ld: %d has taken a fork\n", gettimeinms() - sm->start,
+		sm->philo[0]->num);
+	die(sm);
+	pthread_mutex_unlock(&sm->forks[0]->fork);
+	return (NULL);
+}
+
+void	diner_one(t_simulation *b)
+{
+	pthread_create(&b->philo[0]->name, NULL, thread_routine_one, b);
+	pthread_join(b->philo[0]->name, NULL);
+}
+
+void	*diner(void *a)
+{
+	t_simulation	*b;
+
+	b = (t_simulation *)a;
+	if (inittime(b) == 1)
+		return (NULL);
+	if (b->nbp == 1)
+		diner_one(b);
+	return (NULL);
 }
